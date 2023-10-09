fa12e49cf69bfa44978883eeaea0573099ba2d14
Caruy Chen <caruychen@gmail.com>
2023-10-09 13:57:14 +0300

    remove printing

--- ex02/src/Fixed.cpp
+++ ex02/src/Fixed.cpp
@@ -4,38 +4,30 @@
 /* Constructors and Destructors */
 
 Fixed::Fixed(void): _fixedPointValue(0)
-{
-  std::cout << "Default constructor called" << std::endl;
-}
+{}
 
 Fixed::Fixed(const int value)
 {
-  std::cout << "Int constructor called" << std::endl;
   this->_fixedPointValue = value << this->_fractionalBits;
 }
 
 Fixed::Fixed(const float value)
 {
-  std::cout << "Float constructor called" << std::endl;
   this->_fixedPointValue = roundf(value * (1 << this->_fractionalBits));
 }
 
 Fixed::Fixed(const Fixed &other)
 {
-  std::cout << "Copy constructor called" << std::endl;
   this->operator=(other);
 }
 
 Fixed::~Fixed(void)
-{
-  std::cout << "Destructor called" << std::endl;
-}
+{}
 
 /* Operators Overload */
 
 Fixed &Fixed::operator=(const Fixed &rhs)
 {
-  std::cout << "Copy assignment operator called" << std::endl;
   this->_fixedPointValue = rhs.getRawBits();
   return (*this);
 }
