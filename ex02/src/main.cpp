c02e3f77d4f204f93db81c71ecc249c7700e5bf2
Caruy Chen <caruychen@gmail.com>
2023-10-12 17:14:04 +0300

    richer test set

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -5,17 +5,45 @@ int main(void)
 {
   Fixed a;
   Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-  std::cout << a << std::endl;
-  std::cout << b << std::endl;
   Fixed const c( Fixed( 10.1f ) / Fixed( 2 ) );
-  std::cout << c << std::endl;
-  /*
-  std::cout << ++a << std::endl;
-  std::cout << a << std::endl;
-  std::cout << a++ << std::endl;
-  std::cout << a << std::endl;
-  std::cout << b << std::endl;
-  std::cout << Fixed::max( a, b ) << std::endl;
-  */
+  std::cout << "Arithmetic Operators" << std::endl;
+  std::cout << "5.05f * 2 = " << b << std::endl;
+  std::cout << "10.1f / 2 = " << c << std::endl;
+  std::cout << "10.1f + 2 = " << Fixed( 10.1f ) + Fixed( 2 ) << std::endl;
+  std::cout << "10.1f - 2 = " << Fixed( 10.1f ) - Fixed( 2 ) << std::endl;
+  std::cout << "Comparison Operators" << std::endl;
+  std::cout << "10.1f > 2 = " << ( Fixed( 10.1f ) > Fixed( 2 ) ) << std::endl;
+  std::cout << "10.1f < 2 = " << ( Fixed( 10.1f ) < Fixed( 2 ) ) << std::endl;
+  std::cout << "10.1f >= 2 = " << ( Fixed( 10.1f ) >= Fixed( 2 ) ) << std::endl;
+  std::cout << "10.1f <= 2 = " << ( Fixed( 10.1f ) <= Fixed( 2 ) ) << std::endl;
+  std::cout << "10.1f == 2 = " << ( Fixed( 10.1f ) == Fixed( 2 ) ) << std::endl;
+  std::cout << "10.1f != 2 = " << ( Fixed( 10.1f ) != Fixed( 2 ) ) << std::endl;
+  std::cout << "2 > 10.1f = " << ( Fixed( 2 ) > Fixed( 10.1f ) ) << std::endl;
+  std::cout << "2 < 10.1f = " << ( Fixed( 2 ) < Fixed( 10.1f ) ) << std::endl;
+  std::cout << "2 >= 10.1f = " << ( Fixed( 2 ) >= Fixed( 10.1f ) ) << std::endl;
+  std::cout << "2 <= 10.1f = " << ( Fixed( 2 ) <= Fixed( 10.1f ) ) << std::endl;
+  std::cout << "2 == 10.1f = " << ( Fixed( 2 ) == Fixed( 10.1f ) ) << std::endl;
+  std::cout << "2 != 10.1f = " << ( Fixed( 2 ) != Fixed( 10.1f ) ) << std::endl;
+  std::cout << "10.1f > 10.1f = " << ( Fixed( 10.1f ) > Fixed( 10.1f ) ) << std::endl;
+  std::cout << "10.1f < 10.1f = " << ( Fixed( 10.1f ) < Fixed( 10.1f ) ) << std::endl;
+  std::cout << "10.1f >= 10.1f = " << ( Fixed( 10.1f ) >= Fixed( 10.1f ) ) << std::endl;
+  std::cout << "10.1f <= 10.1f = " << ( Fixed( 10.1f ) <= Fixed( 10.1f ) ) << std::endl;
+  std::cout << "10.1f == 10.1f = " << ( Fixed( 10.1f ) == Fixed( 10.1f ) ) << std::endl;
+  std::cout << "10.1f != 10.1f = " << ( Fixed( 10.1f ) != Fixed( 10.1f ) ) << std::endl;
+  std::cout << "Increment and Decrement Operators" << std::endl;
+  std::cout << "a = " << a << std::endl;
+  std::cout << "++a = " << ++a << std::endl;
+  std::cout << "a = " << a << std::endl;
+  std::cout << "a++ = " << a++ << std::endl;
+  std::cout << "a = " << a << std::endl;
+  std::cout << "--a = " << --a << std::endl;
+  std::cout << "a = " << a << std::endl;
+  std::cout << "a-- = " << a-- << std::endl;
+  std::cout << "a = " << a << std::endl;
+  std::cout << "min and max function overload" << std::endl;
+  std::cout << "Max(10.1f, 2) = " << Fixed::max( Fixed( 10.1f ), Fixed( 2 ) ) << std::endl;
+  std::cout << "Min(10.1f, 2) = " << Fixed::min( Fixed( 10.1f ), Fixed( 2 ) ) << std::endl;
+  std::cout << "Max(2, 10.1f) = " << Fixed::max( Fixed( 2 ), Fixed( 10.1f ) ) << std::endl;
+  std::cout << "Min(2, 10.1f) = " << Fixed::min( Fixed( 2 ), Fixed( 10.1f ) ) << std::endl;
   return 0;
 }
